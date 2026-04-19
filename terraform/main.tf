38c11725fe1ef53dbd7f60100656413de4304844
Priya Uttarkar <pria.ravin@gmail.com>
2026-04-19 16:52:49 -0400

    Remove S3 bucket policy for frontend (OAC)

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,24 +58,6 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
   }
 }
 
-resource "aws_s3_bucket_policy" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
-      },
-    ]
-  })
-
-  depends_on = [aws_s3_bucket_public_access_block.frontend]
-}
 
 # IAM role for Lambda
 resource "aws_iam_role" "lambda_role" {
